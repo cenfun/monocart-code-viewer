3d38da3b3bcd8b93651b605d1802841727a25844
cenfun <cenfun@gmail.com>
2023-05-19 00:12:12 +0800

    supported highlight counter

--- public/data.js
+++ public/data.js
@@ -1,37 +1,48 @@
 window.viewerData = {
+    'formatted': true,
     'coverage': {
-        'totalLines': 57,
+        'totalLines': 58,
         'uncoveredLines': {
-            '3': 'uncovered',
             '4': 'uncovered',
-            '18': 'partial',
-            '19': 'uncovered',
-            '20': 'uncovered',
-            '30': 'partial',
-            '31': 'uncovered',
+            '6': 'uncovered',
+            '20': 'partial',
+            '21': 'uncovered',
+            '22': 'partial',
+            '31': 'partial',
             '32': 'uncovered',
-            '39': 'partial',
-            '40': 'uncovered',
-            '41': 'uncovered'
+            '33': 'uncovered',
+            '40': 'partial',
+            '41': 'uncovered',
+            '42': 'uncovered'
         },
         'uncoveredPieces': {
-            '18': [
+            '20': [
                 {
                     'start': 25,
-                    'end': 32
+                    'end': 34
                 },
                 {
                     'start': 27,
-                    'end': 32
+                    'end': 34
                 }
             ],
-            '30': [
+            '22': [
+                {
+                    'start': 4,
+                    'end': 5
+                },
+                {
+                    'start': 4,
+                    'end': 5
+                }
+            ],
+            '31': [
                 {
                     'start': 18,
                     'end': 19
                 }
             ],
-            '39': [
+            '40': [
                 {
                     'start': 11,
                     'end': 12
@@ -41,35 +52,66 @@ window.viewerData = {
         'executionCounts': {
             '0': [
                 {
-                    'value': 334,
-                    'column': 0
-                }
-            ],
-            '6': [
+                    'column': 0,
+                    'value': 2,
+                    'end': 848
+                },
                 {
-                    'value': 1002,
-                    'column': 0
+                    'column': 0,
+                    'value': 668,
+                    'end': 24
                 }
             ],
             '8': [
                 {
+                    'column': 0,
+                    'value': 2004,
+                    'end': 340
+                }
+            ],
+            '10': [
+                {
+                    'column': 17,
+                    'value': 4,
+                    'end': 151
+                }
+            ],
+            '13': [
+                {
+                    'column': 21,
+                    'value': 668,
+                    'end': 200
+                }
+            ],
+            '16': [
+                {
+                    'column': 17,
                     'value': 2,
-                    'column': 17
+                    'end': 249
                 }
             ],
-            '11': [
+            '25': [
                 {
-                    'value': 334,
-                    'column': 21
+                    'column': 13,
+                    'value': 2,
+                    'end': 807
                 }
             ],
-            '34': [
+            '35': [
                 {
-                    'value': 1000,
-                    'column': 35
+                    'column': 35,
+                    'value': 2000,
+                    'end': 548
+                }
+            ],
+            '55': [
+                {
+                    'column': 16,
+                    'value': 2,
+                    'end': 847
                 }
             ]
         }
     },
-    'content': "function callback() {\n}\n\nfunction other() {\n}\n\nfunction method(v) {\n    // console.log(\"method\", v);\n    if (v === 2) {\n        console.log(v);\n    }\n    if (v % 3 === 0) {\n        callback();\n    }\n    if (v === 3) {\n        console.log(v);\n    }\n\n    return v === 'other' ? ()=>{\n        console.log('never covered');\n    }\n    : other;\n}\n\nconst main = ()=>{\n    // console.log('main');\n    method(1);\n    method(2);\n\n    const a = 10;\n    if (a === 11) {\n        callback();\n    }\n\n    for (let i = 0; i < 1000; i++) {\n        method(i);\n    }\n\n    const f = false;\n    if (f) {\n        console.log('never covered');\n    }\n\n    const {compress, decompress} = window['lz-utils'];\n\n    const str = '📙 Emoji — 😃 💁👌🎍😍';\n\n    console.assert(str === decompress(compress(str)));\n\n}\n;\n\nwindow.onload = ()=>{\n    main();\n}\n;\n"
+    'content': "function callback() {\n\n}\n\nfunction other() {\n\n}\n\nfunction method(v) {\n    // console.log(\"method\", v);\n    if (v === 2) {\n        console.log(v);\n    }\n    if (v % 3 === 0) {\n        callback();\n    }\n    if (v === 3) {\n        console.log(v);\n    }\n\n    return v === 'other' ? () => {\n        console.log('never covered');\n    } : other;\n}\n\nconst main = () => {\n    // console.log('main');\n    method(1);\n    method(2);\n\n    const a = 10;\n    if (a === 11) {\n        callback();\n    }\n\n    for (let i = 0; i < 1000; i++) {\n        method(i);\n    }\n\n    const f = false;\n    if (f) {\n        console.log('never covered');\n    }\n\n    const {\n        compress,\n        decompress\n    } = window['lz-utils'];\n\n    const str = '📙 Emoji — 😃 💁👌🎍😍';\n\n    console.assert(str === decompress(compress(str)));\n\n};\n\nwindow.onload = () => {\n    main();\n};"
 };
--- src/coverage.js
+++ src/coverage.js
@@ -1,11 +1,12 @@
-
-import { RangeSetBuilder } from '@codemirror/state';
+import {
+    RangeSetBuilder, StateEffect, StateField
+} from '@codemirror/state';
 
 import {
-    gutter, GutterMarker, Decoration, ViewPlugin, WidgetType
+    gutter, GutterMarker, Decoration, ViewPlugin, WidgetType, EditorView
 } from '@codemirror/view';
 
-const createPlugin = (handler) => {
+const createPlugin = (handler, eventHandlers = {}) => {
     return ViewPlugin.fromClass(class {
 
         constructor(view) {
@@ -18,7 +19,8 @@ const createPlugin = (handler) => {
             }
         }
     }, {
-        decorations: (v) => v.decorations
+        decorations: (v) => v.decorations,
+        eventHandlers
     });
 };
 
@@ -71,16 +73,136 @@ export const createCoverage = (coverage, extensions) => {
 
     // =====================================================================
 
-    const createCounter = (count) => {
+    // https://codemirror.net/examples/inverted-effect/
+
+    function mapRange(range, change) {
+        const from = change.mapPos(range.from);
+        const to = change.mapPos(range.to);
+        let undef;
+        return from < to ? {
+            from, to
+        } : undef;
+    }
+
+    const addHighlight = StateEffect.define({
+        map: mapRange
+    });
+    const removeHighlight = StateEffect.define({
+        map: mapRange
+    });
+
+    const highlight = Decoration.mark({
+        class: 'cm-highlight-bg'
+    });
+
+    function cutRange(ranges, r) {
+        const leftover = [];
+        ranges.between(r.from, r.to, (from, to, deco) => {
+            if (from < r.from) {
+                leftover.push(deco.range(from, r.from));
+            }
+            if (to > r.to) {
+                leftover.push(deco.range(r.to, to));
+            }
+        });
+        return ranges.update({
+            filterFrom: r.from,
+            filterTo: r.to,
+            filter: () => false,
+            add: leftover
+        });
+    }
+
+    function addRange(ranges, r) {
+        ranges.between(r.from, r.to, (from, to) => {
+            if (from < r.from) {
+                r = {
+                    from, to: r.to
+                };
+            }
+            if (to > r.to) {
+                r = {
+                    from: r.from, to
+                };
+            }
+        });
+        return ranges.update({
+            filterFrom: r.from,
+            filterTo: r.to,
+            filter: () => false,
+            add: [highlight.range(r.from, r.to)]
+        });
+    }
+
+    const highlightedRanges = StateField.define({
+        create() {
+            return Decoration.none;
+        },
+        update(ranges, tr) {
+            ranges = ranges.map(tr.changes);
+            for (const e of tr.effects) {
+                if (e.is(addHighlight)) {
+                    ranges = addRange(ranges, e.value);
+                } else if (e.is(removeHighlight)) {
+                    ranges = cutRange(ranges, e.value);
+                }
+            }
+            return ranges;
+        },
+        provide: (field) => EditorView.decorations.from(field)
+    });
+
+    extensions.push(highlightedRanges);
+
+    // =====================================================================
+
+    const counterEventHandler = (e, view, hover) => {
+        const target = e.target;
+        if (target.className !== 'cm-counter') {
+            return;
+        }
+
+        const index = target.getAttribute('index');
+
+        const pos = view.posAtDOM(target);
+        const line = view.state.doc.lineAt(pos);
+        const lineIndex = line.number - 1;
+        const list = currentCoverage.executionCounts[lineIndex];
+        if (!list) {
+            return;
+        }
+
+        const item = list[index];
+        if (!item) {
+            return;
+        }
+
+        // console.log(hover, item.end);
+
+        const highlightEffect = hover ? addHighlight : removeHighlight;
+        view.dispatch({
+            effects: highlightEffect.of({
+                from: pos,
+                to: item.end
+            })
+        });
+    };
+
+    const createCounter = (count, index) => {
         const counter = new WidgetType();
         counter.toDOM = function() {
             const wrap = document.createElement('span');
             wrap.className = 'cm-counter';
+            wrap.setAttribute('index', index);
             wrap.innerHTML = `x${count}`;
             return wrap;
         };
+        counter.ignoreEvent = () => {
+            return false;
+        };
         return counter;
     };
+
 
     const coverageCount = createPlugin((view) => {
         const widgets = [];
@@ -92,11 +214,11 @@ export const createCoverage = (coverage, extensions) => {
                 // console.log('line index', lineIndex);
                 const list = currentCoverage.executionCounts[lineIndex];
                 if (list) {
-                    list.forEach((v) => {
+                    list.forEach((v, index) => {
                         const offset = line.from + v.column;
                         if (offset >= from && offset <= to) {
                             const deco = Decoration.widget({
-                                widget: createCounter(v.value),
+                                widget: createCounter(v.value, index),
                                 side: 1
                             });
                             widgets.push(deco.range(offset));
@@ -108,6 +230,13 @@ export const createCoverage = (coverage, extensions) => {
         }
         // console.log('widgets', widgets);
         return Decoration.set(widgets);
+    }, {
+        mouseover: (e, view) => {
+            counterEventHandler(e, view, true);
+        },
+        mouseout: (e, view) => {
+            counterEventHandler(e, view, false);
+        }
     });
 
     extensions.push(coverageCount);
@@ -115,7 +244,7 @@ export const createCoverage = (coverage, extensions) => {
     // =====================================================================
 
     const uncoveredBg = Decoration.mark({
-        class: 'cm-bg-uncovered'
+        class: 'cm-uncovered-bg'
     });
 
     const coverageBg = createPlugin((view) => {
