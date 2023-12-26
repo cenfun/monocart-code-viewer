6b90022da1122ed1ea28cd19d9a5d7c7b7a7021a
cenfun <cenfun@gmail.com>
2023-12-26 13:23:26 +0800

    fix selection cursor

--- src/index.js
+++ src/index.js
@@ -180,6 +180,7 @@ export const createCodeViewer = (container, report) => {
                     ... options
                 }),
                 selection: EditorSelection.create([
+                    EditorSelection.cursor(start),
                     EditorSelection.range(start, end)
                 ])
             });
